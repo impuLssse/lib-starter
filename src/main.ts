1c18c81a4f836427ffe59cd68d9573483b19e0ee
impuLssse <zitpot.ru@gmail.com>
2023-09-24 19:17:39 +0300

    last

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 export function f() {
   console.log("12");
+  console.log("12");
+  console.log("12");
 }
 
 f();
 
 export interface Fight {}
 export interface Fighter {}
+
+import { FightOptionsParallel } from "@types";
